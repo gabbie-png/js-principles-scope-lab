2b7d7bff4079900f5fe85c09d301cc669bb971dd
Gabriella RoseMarino <Gabriellarosemarino@gmail.com>
2021-04-13 22:50:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+//setBestCustomer(bestCustomer);
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+
+}
+//overwriteBestCustomer(bestCustomer);
+
+const leastFavoriteCustomer = "Pedro";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Harry";
+}
+
